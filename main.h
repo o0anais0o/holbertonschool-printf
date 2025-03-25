fb3e24c4791888dff56a71dd313f56a51f06021b
Proser-V <10530@hotbertonstudents.com>
2025-03-25 10:37:55 +0100

    Creating a _print_digit function to handle %d and %i, and update the main.h header file to add the new functions

--- main.h
+++ main.h
@@ -4,29 +4,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-/**
- * struct conv_spe - A structure containing conversion specifier for _printf.
- *
- * Description: This structure contains :
- *                  - Every conversion specifier handeled by _printf function.
- *                  - For each specifier, a function pointer pointing to the
- *                    function called by the specifier.
- *
- * @specifier: The conversion specifier.
- * @func: The function associated, taking for input :
- *            - The format string passed to _printf.
- *            - The arguments list passed to _printf.
- */
-
-typedef struct conv_spe
-{
-	char *specifier;
-	int (*func)(va_list);
-} conversion_specifier;
-
 int _printf(const char *format, ...);
-int _print_string(va_list);
-int _print_char(va_list);
+int _print_string(va_list arguments);
+int _print_char(va_list arguments);
 int _putchar(char c);
+int egde_check(int digit);
+char *rev_string(char *number_str, int length);
+int _print_digit(va_list arguments);
 
 #endif
